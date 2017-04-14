1caff4e74ced9f223d241fbe68c5842e01be5a46
Ethan Favia <efavia215@live.com>
2017-04-14 16:23:36 -0400

    updated package.json

--- playground/callback.js
+++ playground/callback.js
@@ -1,1 +1,15 @@
-var getUser = (id, callback) => 
+var getUser = (id, callback) => {
+    var user = {
+        id: id, 
+        name: 'Arnold'
+    };
+
+    setTimeout(() => {
+        callback(user);
+    }, 3000);
+};
+
+
+getUser(21, (user) => {
+    console.log(user)
+});
