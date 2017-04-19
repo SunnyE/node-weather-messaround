df61e23c03dae11e40987c083c84ac3e2b17dcfb
Ethan Favia <efavia215@live.com>
2017-04-19 10:52:37 -0400

    More printed aspects of the location

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2 ));
+    console.log(JSON.stringify(`Address: ${body.results[0].formatted_address}`));
+    console.log(JSON.stringify(`Latitude: ${body.results[0].geometry.location.lat}`));
+    console.log(JSON.stringify(`Longitude: ${body.results[0].geometry.location.lng}`));
 });
